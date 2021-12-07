bc057af778af8905460c930bf5b08ef257b98c9a
Ramon Sistermans <ramon.sistermans@incentro.com>
2021-12-07 12:16:21 +0100

    Day 7-2 improved performance

--- day-07/part-2.py
+++ day-07/part-2.py
@@ -4,18 +4,32 @@ with open('input.txt') as f:
 
 def solution(numbers):
 
-    max_pos = max(numbers)
+    distance = get_closest_distance(numbers)
 
-    options = []
+    fuel = 0
+    for n in numbers:
+        dist = abs(n - distance)
+        fuel += sum(range(dist + 1))
 
-    for i in range(1, max_pos + 1):
-        fuel = 0
-        for n in numbers:
-            dist = abs(n - i)
-            fuel += sum(range(dist+1))
-        options.append(fuel)
+    print(fuel)
 
-    print(min(options))
+
+def get_closest_distance(numbers):
+    unique_numbers = list(set(numbers))
+    max_pos = max(unique_numbers)
+    min_pos = min(unique_numbers)
+    indices = [i for i in range(min_pos, max_pos + 1)]
+
+    distance_sums = []
+
+    for i in indices:
+        distance = 0
+        for n in unique_numbers:
+            distance += abs(n - i)
+        distance_sums.append(distance)
+
+    closest_distance = min(distance_sums)
+    return distance_sums.index(closest_distance) + 1
 
 
 solution(lines)
